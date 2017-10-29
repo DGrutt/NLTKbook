293e6e1443dc6876d1f5bb840c5463b975a1c74e
DGrutt <dg36@buffalo.edu>
2017-10-28 20:03:56 -0400

    homework ch02 question 1
    homework ch02 question 1

---
+++ Ch02.01.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+# In[6]:
+
+
+phrase = ["this", "is", "a", "phrase"]
+phrase2 = phrase + phrase #addition
+phrase3 = phrase * 4 #multiplication
+phrase4 = phrase[2]#indexing
+phrase5 = phrase[:2]#slicing
+phrase6 = sorted(phrase)#sorting
+
+print (phrase2)
+print (phrase3)
+print (phrase4)
+print (phrase5)
+print (phrase6)
+
